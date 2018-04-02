5efc591dd43a177ca40b59e3a233dc51719e575e
catalina <catapf493@yahoo.com>
2018-04-02 07:45:06 -0500

    functional docker-compose

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     AUTH0_DOMAIN: 'agudelotmateo.auth0.com',
     AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', 
-    //MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/tracker-v2'
-    MONGO_URI: 'mongodb://localhost:27017/tracker-v2'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://database/tracker-v2'
+    // MONGO_URI: 'mongodb://localhost:27017/tracker-v2'
     // MONGO_URI: 'mongodb://database/tracker-v2'
 };
